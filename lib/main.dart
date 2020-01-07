1dcd32417f304e24a9e9997f32a2ea667c374aa8
Aurelio <aurelio@sparanzza.com>
2020-01-07 21:24:50 +0100

    First Commit, home temp

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_components/src/pages/home_page.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Components App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Components App Bar'),
+        ),
+        body: HomePageTemp()
+      ),
+    );
+  }
+}
