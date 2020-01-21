7c2a51f2988ce8a3a54fd4c8edf0cf105c196f99
Aurelio <aurelio@sparanzza.com>
2020-01-21 22:36:09 +0100

    Add simple navigation pages

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/src/pages/alert_page.dart';
 import 'package:flutter_components/src/providers/menu_provider.dart';
 import 'package:flutter_components/src/utils/icons_strings_util.dart';
 
@@ -21,20 +22,24 @@ class HomePage extends StatelessWidget {
         print('builder');
         print(snapshot.data);
         return ListView(
-          children: _listItems(snapshot.data),
+          children: _listItems(snapshot.data, context),
         );
       },
     );
   }
 
-  List<Widget> _listItems(List<dynamic> data) {
+  List<Widget> _listItems(List<dynamic> data, BuildContext context) {
     final List<Widget> opts = [];
     data.forEach((opt) {
       final wtemp = ListTile(
         title: Text(opt['texto']),
         leading: getIcon(opt['icono']),
         trailing: Icon(Icons.arrow_forward_ios, color: Colors.blue,),
-        onTap: (){},
+        onTap: (){
+          final route = MaterialPageRoute(
+            builder:(context) => AlertPage());
+          Navigator.push(context, route);
+        },
       );
       opts..add(wtemp)
           ..add(Divider());
